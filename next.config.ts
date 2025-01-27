fdf0a7a84b7bcfbd82a762c2020aa2cabcbe695d
marcelmunevar-contractor-fujifilm <66744949+marcelmunevar-contractor-fujifilm@users.noreply.github.com>
2025-01-27 16:37:09 -0500

    unoptimized, true

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const isProduction = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  images: {
+    unoptimized: true,
+  },
   basePath: isProduction ? "/cell-therapy" : "", // Apply basePath only in production
   assetPrefix: isProduction ? "/cell-therapy" : "", // Apply assetPrefix only in production
 };
