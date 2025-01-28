ffffdacfad1c101c694138449922bec9919b41f6
marcelmunevar-contractor-fujifilm <66744949+marcelmunevar-contractor-fujifilm@users.noreply.github.com>
2025-01-28 16:53:10 -0500

    changed style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
         </div>
       </div>
       <div
-        className="w-full h-1 bg-gradient-to-r from-transparent to-transparent mb-8"
+        className="w-full h-2 bg-gradient-to-r from-transparent to-transparent mb-8"
         style={{
           background:
             "linear-gradient(90deg, #00d164 0%, #00d164 5%, #00ac5a 9%, #008551 14%, #006c4b 18%, #006449 20%, #007a5b 29%, #008b68 38%, #01916d 45%, #00956b 51%, #00a168 56%, #00b663 62%, #00d35c 68%, #00f853 74%, #00ff52 75%, #96fffd 95%, #96fffd 100%)",
