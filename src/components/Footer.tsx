b25a5cd8359965039c5c751a1cfd70a781ff8297
Mark Yudt (Contractor) <Mark.Yudt@004.fujifilm.com>
2025-01-29 14:04:36 -0500

    adjusted mobile styles and text sizes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,28 +22,28 @@ export default function Footer() {
         
         <div className="md:px-10">
           <div className="flex flex-col md:flex-row ">
-            <div className="md:after:content-[''] md:after:border-r after:border-[#AEADAD] after:text-base pb-4 md:pb-2">
+            <div className="md:after:content-[''] md:after:border-r after:border-[#AEADAD] text-xs xl:text-[18px] pb-4">
               <a className="md:pr-4" href="#">
                 Privacy Policy
               </a>
             </div>
-            <div className="md:after:content-[''] md:after:border-r after:border-[#AEADAD] after:text-base pb-4 md:pb-2">
+            <div className="md:after:content-[''] md:after:border-r after:border-[#AEADAD] text-xs xl:text-[18px] pb-4">
               <a className="md:px-4" href="#">
                 Terms of Use
               </a>
             </div>
-            <div className="md:after:content-[''] md:after:border-r after:border-[#AEADAD] after:text-base pb-4 md:pb-2">
+            <div className="md:after:content-[''] md:after:border-r after:border-[#AEADAD] text-xs xl:text-[18px] pb-4">
               <a className="md:px-4" href="#">
                 Contact us
               </a>
             </div>
-            <div className="pb-8 md:pb-2">
+            <div className="text-xs xl:text-[18px] pb-8 md:pb-4">
               <a className="md:px-4" href="#">
                 Cookie Settings
               </a>
             </div>
           </div>
-          <div className="text-xs mb-4">©FUJIFILM Holdings Corporation</div>
+          <div className="text-xs xl:text-sm mb-4">©FUJIFILM Holdings Corporation</div>
         </div>
 
       </div>
